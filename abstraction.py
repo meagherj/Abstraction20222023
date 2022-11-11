7fad13140897d9a45feaf7589dd8d99baa73d013
hu-emily <70549106+hu-emily@users.noreply.github.com>
2022-11-11 09:17:53 -0600

    Update abstraction.py

--- abstraction.py
+++ abstraction.py
@@ -1,27 +1,13 @@
+d = {1:10, 2:20, 3:30, 4:40, 5:50}
+path = {1:[d[1], d[2], d[3]], 2:[d[2], d[3], d[4]], 3:[d[5], d[4], d[3]]}
 
-distance1 = 10
-distance2 = 20
-distance3 = 30
-distance4 = 40
-distance5 = 50
+def driveToDistance(a):
+    print(d[a])
 
-path1 = [distance1,distance2,distance3]
-path2 = [distance2,distance3,distance4]
-path3 = [distance5,distance4,distance3]
+def followPath(b):
+    print(path[b])
 
-def driveToDistance1():
-    print(distance1)
-
-def driveToDistance2():
-    print(distance2)
-
-def followPath1():
-    print(path1)
-
-def followPath2():
-    print(path2)
-
-driveToDistance1()
-driveToDistance2()
-followPath1()
-followPath2()
+driveToDistance(1)
+driveToDistance(2)
+followPath(1)
+followPath(2)
