eab1f7d5a85608b244d285748d1c2f47b5ae3379
titush6002 <116006591+titush6002@users.noreply.github.com>
2022-11-11 09:49:27 -0600

    Update abstraction.py

--- abstraction.py
+++ abstraction.py
@@ -1,6 +1,6 @@
 dis_dict = {'distance1':10,'distance2':20,'distance3':30,'distance4':40,'distance5':50,}
             
-path_dict = {'path1':[dis_dict['distance1'],dis_dict['distance2'],dis_dict['distance3']],'path2':[dis_dict['distance2'],dis_dict['distance3'],dis_dict['distance4']],'path3':[dis_dict['distance5'],dis_dict['distance4'],dis_dict['distance3']]}
+path_dict = {'path1':[dis_dict['distance1'] , dis_dict['distance2'] , dis_dict['distance3']] , 'path2':[dis_dict['distance2'] , dis_dict['distance3'] , dis_dict['distance4']] , 'path3':[dis_dict['distance5'] , dis_dict['distance4'] , dis_dict['distance3']]}
       
 def driveToDistance(dis_dict):
     print(dis_dict)
