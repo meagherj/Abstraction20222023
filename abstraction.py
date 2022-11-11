6a7c627b3c36a4bb045be961e4e869fcfb4e8510
Cwilliams68419 <116006731+Cwilliams68419@users.noreply.github.com>
2022-11-11 09:45:23 -0600

    AAAAAAAAAAA
    akefhae

--- abstraction.py
+++ abstraction.py
@@ -1,27 +1,8 @@
-
-distance1 = 10
-distance2 = 20
-distance3 = 30
-distance4 = 40
-distance5 = 50
-
-path1 = [distance1,distance2,distance3]
-path2 = [distance2,distance3,distance4]
-path3 = [distance5,distance4,distance3]
-
-def driveToDistance1():
-    print(distance1)
-
-def driveToDistance2():
-    print(distance2)
-
-def followPath1():
-    print(path1)
-
-def followPath2():
-    print(path2)
-
-driveToDistance1()
-driveToDistance2()
-followPath1()
-followPath2()
+distances=[10,20,30,40,50]
+pathes = [(distances[0],distances[1],distances[2])],[(distances[1],distances[2],distances[3])]
+def printthing(x):
+    print(x)
+printthing(distances[0])
+printthing(distances[1])
+printthing(pathes[0])
+printthing(pathes[1])
