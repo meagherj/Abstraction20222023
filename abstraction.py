20cf38c8d62d7d1cc1659812f47a90853553f890
michelleakins <91162069+michelleakins@users.noreply.github.com>
2022-11-11 09:45:57 -0600

    Update abstraction.py

--- abstraction.py
+++ abstraction.py
@@ -1,27 +1,17 @@
+import os
+os.system('cls')
 
-distance1 = 10
-distance2 = 20
-distance3 = 30
-distance4 = 40
-distance5 = 50
+distances = [10,20,30,40]
 
-path1 = [distance1,distance2,distance3]
-path2 = [distance2,distance3,distance4]
-path3 = [distance5,distance4,distance3]
+allpaths = {1:[distances[0], distances[1], distances[2]], 2:[distances[1], distances[2], distances[3]]}
 
-def driveToDistance1():
-    print(distance1)
-
-def driveToDistance2():
-    print(distance2)
-
+def driveToDistances(distance):
+    print(distance)
+    
 def followPath1():
-    print(path1)
+    print("path 1:",allpaths[1])
+    print("path 2:",allpaths[2])
 
-def followPath2():
-    print(path2)
-
-driveToDistance1()
-driveToDistance2()
+driveToDistances(distances[1])
+driveToDistances(distances[2])
 followPath1()
-followPath2()
