770424dcdb57e47302b495d7389c106888fb85c2
KFry101 <97113447+KFry101@users.noreply.github.com>
2022-11-11 09:40:41 -0600

    abstraction katie

--- abstraction.py
+++ abstraction.py
@@ -1,6 +1,6 @@
 distances= {1:10, 2:20, 3:30, 4:40, 5:50}
 
-paths= {1:[distance[1],distance[2],distance[3]], 2:[distance[2],distance[3],distance[4]], 3:[distance[5],distance[4],distance[3]]}
+paths= {1:[distances[1],distances[2],distances[3]], 2:[distances[2],distances[3],distances[4]], 3:[distances[5],distances[4],distances[3]]}
 
 def driveToDistance(Distance):
     print(Distance)
