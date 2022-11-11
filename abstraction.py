c27a5a5317a11b3c00fff1278ed1748869f749ac
TalRogozinski <116005072+TalRogozinski@users.noreply.github.com>
2022-11-11 09:37:07 -0600

    Update abstraction.py

--- abstraction.py
+++ abstraction.py
@@ -1,5 +1,5 @@
 distances={'distance1': 10,'distance2': 20,'distance3':30,'distance4': 40, 'distance5': 50}
-paths={'path1': [distances ['distance1'] , distances ['distance2'] , distances ['distance2']], 'path2': [distances ['distance2'], distances ['distance3'], distances ['distance4']], 'path3': [distances ['distance5'], distances['distance4'], distances['distance3']}
+paths={'path1': [distances ['distance1'] , distances ['distance2'] , distances ['distance2']], 'path2': [distances ['distance2'], distances ['distance3'], distances ['distance4']], 'path3': [distances ['distance5'], distances['distance4'], distances['distance3']]}
 
 
 def driveToDistances(distance):
