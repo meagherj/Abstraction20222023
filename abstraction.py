44cb9f0cf833e95cd884fe3a574e52ebcd22bbe2
titush6002 <116006591+titush6002@users.noreply.github.com>
2022-11-11 09:45:45 -0600

    Update abstraction.py

--- abstraction.py
+++ abstraction.py
@@ -1,6 +1,6 @@
 dis_dict = {'distance1':10,'distance2':20,'distance3':30,'distance4':40,'distance5':50,}
             
-path_dict = {'path1':dis_dict['distance1'],['distance2'],['distance3'],'path2':dis_dict['distance2'],['distance3'],['distance4'],'path3':dis_dict['distance5'],['distance4'],['distance3']}
+path_dict = {'path1':[dis_dict['distance1'],dis_dict['distance2'],dis_dict['distance3']],'path2':[dis_dict['distance2'],dis_dict['distance3'],dis_dict['distance4']],'path3':[dis_dict['distance5'],dis_dict['distance4'],dis_dict['distance3']]}
       
 def driveToDistance(dis_dict):
     print(dis_dict)
@@ -9,8 +9,8 @@ def followPath(path_dict):
     print(path_dict)
 
 
-driveToDistance(dis_dict[distance1])
-driveToDistance(dis_dict[distance2])
+driveToDistance(dis_dict['distance1'])
+driveToDistance(dis_dict['distance2'])
 
-followPath(path_dict[path1])
-followPath(path_dict[path2])
+followPath(path_dict['path1'])
+followPath(path_dict['path2'])
