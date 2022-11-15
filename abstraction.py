d89ece42ebfebefa7801f9ae5df4d77ada6f53cf
sahad24greenhill <112664636+sahad24greenhill@users.noreply.github.com>
2022-11-15 08:45:40 -0600

    Update abstraction.py
    Incorrect variable names have been fixed.

--- abstraction.py
+++ abstraction.py
@@ -3,13 +3,13 @@ driveDistances = {1:10, 2:20, 3:30, 4:40, 5:50}
 drivePaths = {1:[10, 20, 30], 2:[20, 30, 40]}
 
 # Define Drive + Path Functions
-def driveToDistance(distance):
-    print(distance)
-def followPath(path):
-    print(path)
+def driveToDistance(driveDistances):
+    print(driveDistances)
+def followPath(drivePaths):
+    print(drivePaths)
     
 # Output Distance + Path Values
-driveToDistance(distances[1])
-driveToDistance(distances[2])
-followPath(path[1])
-followPath(path[2])
+driveToDistance(driveDistances[1])
+driveToDistance(driveDistances[2])
+followPath(drivePaths[1])
+followPath(drivePaths[2])
