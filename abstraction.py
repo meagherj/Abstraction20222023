9a6eb2f0b7423140a138f5581f852e11b3f49db6
sahad24greenhill <112664636+sahad24greenhill@users.noreply.github.com>
2022-11-15 08:39:20 -0600

    Update abstraction.py
    Created functions and dictionaries to make the code more efficient (i.e. using abstraction).

--- abstraction.py
+++ abstraction.py
@@ -1,27 +1,15 @@
+# Declare Distance + Path Values
+driveDistances = {1:10, 2:20, 3:30, 4:40, 5:50}
+drivePaths = {1:[10, 20, 30], 2:[20, 30, 40]}
 
-distance1 = 10
-distance2 = 20
-distance3 = 30
-distance4 = 40
-distance5 = 50
-
-path1 = [distance1,distance2,distance3]
-path2 = [distance2,distance3,distance4]
-path3 = [distance5,distance4,distance3]
-
-def driveToDistance1():
-    print(distance1)
-
-def driveToDistance2():
-    print(distance2)
-
-def followPath1():
-    print(path1)
-
-def followPath2():
-    print(path2)
-
-driveToDistance1()
-driveToDistance2()
-followPath1()
-followPath2()
+# Define Drive + Path Functions
+def driveToDistance(distance):
+    print(distance)
+def followPath(path):
+    print(path)
+    
+# Output Distance + Path Values
+driveToDistance(distances[1])
+driveToDistance(distances[2])
+followPath(path[1])
+followPath(path[2])Creat
