f9c2b257c9c0e8608d8b51b9763066db3562e4da
nehabachu <89409075+nehabachu@users.noreply.github.com>
2022-11-30 08:15:09 -0600

    Update abstraction.py

--- abstraction.py
+++ abstraction.py
@@ -1,27 +1,16 @@
 
-distance1 = 10
-distance2 = 20
-distance3 = 30
-distance4 = 40
-distance5 = 50
+distanceList = [10,20,30,40,50]
 
-path1 = [distance1,distance2,distance3]
-path2 = [distance2,distance3,distance4]
-path3 = [distance5,distance4,distance3]
+paths = {
+    "path1":[distanceList[0],distanceList[1],distanceList[2]],
+    "path2":[distanceList[1],distanceList[2],distanceList[3]],
+    "path3":[distanceList[4],distanceList[3],distanceList[2]]
+    }
 
-def driveToDistance1():
+def driveToDistance(distance1):
     print(distance1)
 
-def driveToDistance2():
-    print(distance2)
-
-def followPath1():
-    print(path1)
-
-def followPath2():
-    print(path2)
-
-driveToDistance1()
-driveToDistance2()
-followPath1()
-followPath2()
+driveToDistance(distanceList[0])
+driveToDistance(distanceList[1])
+driveToDistance(paths["path1"])
+driveToDistance(paths["path2"])
